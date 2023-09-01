cbdc2270ec4bc56e4bd2aab2360af2c8e7e26961
VaasuDevanS <vaasuceg.96@gmail.com>
2023-09-01 10:52:17 -0300

    command line tool and updated readme

--- README.md
+++ README.md
@@ -2,22 +2,29 @@
 
 ## Instructions
 1. Clone this Repository
-2. The script depends on `requests`, `beautifulsoup`, `tqdm` and `re`. See `requirements.txt` to create a new conda environment or you can install these packages directly using `pip install requests beautifulsoup4 tqdm` in your desired environment
-3. Set the `url` to the podcast and `out_dir` within the script ([lines 16-17](https://github.com/VaasuDevanS/google-podcast-downloader/blob/c09068092d8807a61fe860ad290517303ceead3e/google-podcast-downloader.py#L16-L17))
-3.5 Alternatively, simply run `python3 google-podcast-downloader.py "<url_to_podcast>" "<Out Directory>"`
-4. Run the script using `python google-podcast-downloader.py`
-5. [Optional] You can change the file name template in [line 44](https://github.com/VaasuDevanS/google-podcast-downloader/blob/c09068092d8807a61fe860ad290517303ceead3e/google-podcast-downloader.py#L44). The default template is `Ep 000 - <Podcast-Name> (<Date-Published>)`
+2. Use `requirements.txt` or install using `pip install beautifulsoup4 requests tqdm` in your environment
+3. Run `python google-podcast-downloader.py --url "" --out-dir ""`
+
+
+## Usage
+```
+$ python google-podcast-downloader.py -h
+
+usage: Google Podcast Downloader [-h] [--url URL] [--out-dir OUT_DIR]
+
+Script to download entire Podcast Library
+
+options:
+  -h, --help         show this help message and exit
+  --url URL          URL of the podcast
+  --out-dir OUT_DIR  Folder to download
+```
 
 
 ## Tested Podcasts
-
-I personally used the script to download the below podcasts for offline listening.
-
-<a href="https://podcasts.google.com/feed/aHR0cHM6Ly9hbmNob3IuZm0vcy85NDFlMWQ4L3BvZGNhc3QvcnNz?sa=X&ved=2ahUKEwit0NGrhoOBAxVIE1kFHeEhBaMQ9sEGegQIARAE"><img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTm27nk43UwjX3vZclxYjmkdot4VLYMMcpdp8VDzXaix0YmAwU" width=250 height=250></a>
+<a href="https://podcasts.google.com/feed/aHR0cHM6Ly9weXRob25ieXRlcy5mbS9lcGlzb2Rlcy9yc3M"><img src="https://github.com/VaasuDevanS/google-podcast-downloader/assets/24793046/356b195c-3602-4bac-bda4-efa797eb5f08" width=250 height=250></a>
 <a href="https://podcasts.google.com/feed/aHR0cHM6Ly90YWxrcHl0aG9uLmZtL2VwaXNvZGVzL3Jzcw"><img src="https://user-images.githubusercontent.com/24793046/188334448-f50171b9-8501-426e-83f7-553feb376369.png" width=250 height=250></a>
 <a href="https://podcasts.google.com/feed/aHR0cHM6Ly9yZWFscHl0aG9uLmNvbS9wb2RjYXN0cy9ycHAvZmVlZA=="><img src="https://user-images.githubusercontent.com/24793046/188514673-8eaa6c57-338a-46fe-8ae8-160718bb4900.png" width=250 height=250></a>
-<a href="https://podcasts.google.com/feed/aHR0cHM6Ly9tYXBzY2FwaW5nLnBvZGJlYW4uY29tL2ZlZWQueG1s"><img src="https://user-images.githubusercontent.com/24793046/188515031-1c71a2a1-331d-4dec-aafb-34f53f1aa47d.png" width=250 height=250></a>
-
 
 ## Contributions
 If you like this work, give a ⭐  
--- google-podcast-downloader.py
+++ google-podcast-downloader.py
@@ -1,86 +1,61 @@
-#!/usr/bin/env python3
 """
-Developer: Vaasudevan Srinivasan
-Created on: Aug 25, 2022
+Created: Aug 25, 2022
+Last Modified: Sep 01, 2023
 Description: Script to download entire Podcast Library for the given url
-Email: vaasuceg.96@gmail.com
 Github: https://github.com/VaasuDevanS/google-podcast-downloader
-Pull Request by: HirschBerge
-Date Submitted: 08/29/23
 """
+
+import argparse
+from pathlib import Path
+import re
 
 from bs4 import BeautifulSoup
-import re
 import requests
 from tqdm import tqdm
-import os
-import sys
-
-if len(sys.argv) == 3:
-    url = sys.argv[1]
-    print("URL:", url)
-    out_dir = sys.argv[2]
-    print("Out Dir:", out_dir)
-elif len(sys.argv) == 2:
-    out_dir = r""  # fill in the directory to put your files into.
-    url = sys.argv[1]
-else:
-    url = r""  # Fill in your URL here if you don't want to pass cli args.
-    out_dir = r""  # fill in the directory to put your files into.
 
 
-class NotEnoughParams(Exception):
-    def __init__(self, message):
-        self.message = message
+def main(url: str, out_dir: Path) -> None:
 
-
-def check_input(out_dir, url):
-    if "~" in out_dir:
-        out_dir = os.path.expanduser(out_dir)
-
-    if not os.path.exists(out_dir):
-        try:
-            os.makedirs(out_dir)
-        except Exception as e:
-            raise ValueError(f"Invalid path: {out_dir}")
-    return out_dir
-
-
-if __name__ == "__main__":
-    if not out_dir or not url:
-        raise NotEnoughParams("Please provide both a URL and an output directory.")
-    out_dir = check_input(out_dir, url)
+    # Create directory if it doesn't exist
+    out_dir.mkdir(parents=True, exist_ok=True)
 
     # Read the url and create soup object
     r = requests.get(url)
-    soup = BeautifulSoup(r.content, "html.parser")
+    soup = BeautifulSoup(r.content, 'html.parser')
 
     # Get all the divs corresponding to each podcast episode
-    divs = soup.find_all("div", attrs={"class": "oD3fme"})
+    divs = soup.find_all("div", attrs={'class': 'oD3fme'})
 
     # Iterate through each div (episode)
     for ix, div in tqdm(enumerate(divs[::-1]), total=len(divs)):
-        # Get the date published
-        date = div.find("div", attrs={"class": "OTz6ee"}).text
-        # Get the name of the episode
-        name = div.find("div", attrs={"class": "e3ZUqe"}).text
-        name = re.sub('[\/:*?"<>|]+', "", name)
-        file_name = f"EP {ix:03d} - {name} ({date}).mp3"
 
-        episode_path = os.path.join(out_dir, file_name)
-        if os.path.exists(episode_path):
-            pass  # This file already exists...
-        # Fetch each episode and write the file
-        try:
-            podcast = requests.get(url)
-        except:
-            pass  # Skips this episode.
+        # Get the date published
+        date = div.find("div", attrs={'class': 'OTz6ee'}).text
+
+        # Get the name of the episode
+        name = div.find("div", attrs={'class': 'e3ZUqe'}).text
+        name = re.sub('[/:*?"<>|]+', '', name)
+        file_name = f'EP {ix:03d} - {name} ({date}).mp3'
+        episode_path = out_dir / file_name
 
         # Get the URL
         url = div.find("div", attrs={"jsname": "fvi9Ef"}).get("jsdata")
         url = url.split(";")[1]
 
-        # Construct the File name
-
+        # Download the episode
+        podcast = requests.get(url)
         with open(rf"{episode_path}", "wb") as out:
             out.write(podcast.content)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        prog='Google Podcast Downloader',
+        description='Script to download entire Podcast Library',
+    )
+    parser.add_argument('--url', help='URL of the podcast')
+    parser.add_argument('--out-dir', help='Folder to download')
+
+    args = parser.parse_args()
+    main(url=args.url, out_dir=Path(args.out_dir))
